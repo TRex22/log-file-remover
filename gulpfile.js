8f761008eeae827eecdef827ecb6acf2ab70f616
Jason Chalom <jason.chalom@encentivize.co.za>
2016-01-21 13:49:54 +0200

    added gulp for deployment

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+/*globals require, console, __dirname*/
+'use strict';
+var util = require("util");
+var gulp = require("gulp");
+var jshint = require('gulp-jshint');
+var argv = require('yargs').argv;
+var npm = require('npm');
+var npmPackage = require("./package.json");
+
+gulp.task('lint', function () {
+    return gulp.src(config.appFiles.js)
+        .pipe(jsHint())
+        .pipe(jsHint.reporter('default'))
+        .pipe(jsHint.reporter('fail'));
+});
+
+gulp.task('npmPublish', function (callback) {
+    var username = argv.username;
+    var password = argv.password;
+    var email = argv.email;
+    if (!username) {
+        var usernameError = new Error("Username is required as an argument --username exampleUsername");
+        return callback(usernameError);
+    }
+    if (!password) {
+        var passwordError = new Error("Password is required as an argument --password examplePassword");
+        return callback(passwordError);
+    }
+    if (!email) {
+        var emailError = new Error("Email is required as an argument --email example@email.com");
+        return callback(emailError);
+    }
+    var uri = "http://registry.npmjs.org/";
+    npm.load(null, function (loadError) {
+        if (loadError) {
+            return callback(loadError);
+        }
+        var auth = {
+            username: username,
+            password: password,
+            email: email,
+            alwaysAuth: true
+        };
+        var addUserParams = {
+            auth: auth
+        };
+        npm.registry.adduser(uri, addUserParams, function (addUserError, data, raw, res) {
+            if (addUserError) {
+                return callback(addUserError);
+            }
+            var packageJson = _.clone(require('./package.json'));
+            npm.commands.pack([], function (packError) {
+                if (packError) {
+                    return callback(packError);
+                }
+                var fileName = packageJson.name.substring(1).replace(/\//g, '-') + '-' + packageJson.version + '.tgz';
+                var bodyPath = require.resolve('./' + fileName);
+                var body = fs.createReadStream(bodyPath);
+                var publishParams = {
+                    metadata: packageJson,
+                    access: 'restricted',
+                    body: body,
+                    auth: auth
+                };
+                npm.registry.publish(uri, publishParams, function (publishError, resp) {
+                    if (publishError) {
+                        return callback(publishError);
+                    }
+                    console.log("Publish successful: " + JSON.stringify(resp));
+                    return callback();
+                });
+            });
+        });
+    });
+});
